870e305474230d243c7867f366606cce1557ef9e
Fred Mac <freddymcgroarty@gmail.com>
2023-11-07 01:20:22 +0000

    ACL: additional Api Permissions requested

--- Controller/Adminhtml/Redirect/Index.php
+++ Controller/Adminhtml/Redirect/Index.php
@@ -7,14 +7,15 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-	public function execute()
-	{
-		$encodedUrlSegment = $this->getRequest()->getParam('url');
-		$urlSegment = base64_decode($encodedUrlSegment);
-		$url = 'https://app.addsauce.com/' . $urlSegment; // Concatenate the parameter
+  public function execute()
+  {
 
-		$resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-		$resultRedirect->setUrl($url);
-		return $resultRedirect;
-	}
+    $encodedUrlSegment = $this->getRequest()->getParam('url');
+    $urlSegment = base64_decode($encodedUrlSegment);
+    $url = 'https://app.addsauce.com/' . $urlSegment; // Concatenate the parameter
+
+    $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+    $resultRedirect->setUrl($url);
+    return $resultRedirect;
+  }
 }
