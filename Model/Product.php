37e09dfa1a8777e5d97f92f3ce5d843da993db24
Fred Mac <freddymcgroarty@gmail.com>
2023-11-14 12:44:03 +0000

    Hotfix: Fix magento code sniff warnings
    Also makes a significant change to Redirect/Index.php to allow for a
    magento safe way of decoding base64.
    
    Also adds correct deps into composer.json - this would have failed QA
    otherwise.

--- Model/Product.php
+++ Model/Product.php
@@ -1,63 +1,146 @@
 <?php
 namespace Sauce\App\Model;
 
+/**
+ * Class Product
+ * Represents a product with properties like SKU, URL, visibility, etc.
+ */
 class Product implements \Sauce\App\Api\Data\ProductInterface
 {
+    /**
+     * @var string
+     * The product SKU.
+     */
     protected $sku;
+
+    /**
+     * @var string
+     * The product URL.
+     */
     protected $url;
+
+    /**
+     * @var int
+     * The product visibility status.
+     */
     protected $visibility;
+
+    /**
+     * @var string
+     * The name of the product.
+     */
     protected $name;
+
+    /**
+     * @var array
+     * An array of media gallery entries for the product.
+     */
     protected $mediaGalleryEntries;
 
+    /**
+     * Gets the SKU of the product.
+     *
+     * @return string
+     */
     public function getSku()
     {
         return $this->sku;
     }
 
+    /**
+     * Sets the SKU of the product.
+     *
+     * @param string $sku
+     * @return $this
+     */
     public function setSku($sku)
     {
         $this->sku = $sku;
         return $this;
     }
 
+    /**
+     * Gets the URL of the product.
+     *
+     * @return string
+     */
     public function getUrl()
     {
         return $this->url;
     }
 
+    /**
+     * Sets the URL of the product.
+     *
+     * @param string $url
+     * @return $this
+     */
     public function setUrl($url)
     {
         $this->url = $url;
         return $this;
     }
 
+    /**
+     * Gets the visibility of the product.
+     *
+     * @return int
+     */
     public function getVisibility()
     {
         return $this->visibility;
     }
 
+    /**
+     * Sets the visibility of the product.
+     *
+     * @param int $visibility
+     * @return $this
+     */
     public function setVisibility($visibility)
     {
         $this->visibility = $visibility;
         return $this;
     }
 
+    /**
+     * Gets the name of the product.
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Sets the name of the product.
+     *
+     * @param string $name
+     * @return $this
+     */
     public function setName($name)
     {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * Gets the media gallery entries of the product.
+     *
+     * @return array
+     */
     public function getMediaGalleryEntries()
     {
         return $this->mediaGalleryEntries;
     }
 
+    /**
+     * Sets the media gallery entries for the product.
+     *
+     * @param string $mediaGalleryEntries
+     * @return $this
+     */
     public function setMediaGalleryEntries($mediaGalleryEntries)
     {
         $this->mediaGalleryEntries = $mediaGalleryEntries;
--- Model/ProductApi.php
+++ Model/ProductApi.php
@@ -8,13 +8,41 @@ use Sauce\App\Helper\Version;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaInterface;
 
+/**
+ * Product API class
+ *
+ * Provides functionality for retrieving product information.
+ */
 class ProductApi implements ProductApiInterface
 {
+    /**
+     * @var ProductInterfaceFactory Factory for creating product data instances.
+     */
     protected $productInterfaceFactory;
+
+    /**
+     * @var ProductSearchResultsInterfaceFactory Factory for creating product search results instances.
+     */
     protected $productSearchResultsInterfaceFactory;
+
+    /**
+     * @var ProductRepositoryInterface Repository for product data.
+     */
     protected $productRepository;
+
+    /**
+     * @var Version Helper for managing version information.
+     */
     protected $versionHelper;
 
+    /**
+     * Constructor for ProductApi.
+     *
+     * @param ProductInterfaceFactory $productInterfaceFactory
+     * @param ProductSearchResultsInterfaceFactory $productSearchResultsInterfaceFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param Version $versionHelper
+     */
     public function __construct(
         ProductInterfaceFactory $productInterfaceFactory,
         ProductSearchResultsInterfaceFactory $productSearchResultsInterfaceFactory,
@@ -27,12 +55,18 @@ class ProductApi implements ProductApiInterface
         $this->versionHelper = $versionHelper;
     }
 
+    /**
+     * Retrieves a list of products based on the given search criteria.
+     *
+     * @param SearchCriteriaInterface $searchCriteria Criteria for filtering products.
+     * @return mixed Search results containing product data.
+     */
     public function getList(SearchCriteriaInterface $searchCriteria)
     {
         $searchResults = $this->productSearchResultsInterfaceFactory->create();
         $searchResults->setSearchCriteria($searchCriteria);
 
-      // Get the module version
+        // Get the module version
         $version = $this->versionHelper->getModuleVersion('Sauce_App');
         $extensionVersion = $version ?? 'unknown';
         $searchResults->setExtensionVersion($extensionVersion);
@@ -48,7 +82,7 @@ class ProductApi implements ProductApiInterface
             $productData->setName($product->getName());
             $productData->setUrl($product->getProductUrl());
 
-          // Get media gallery entries for each product
+            // Get media gallery entries for each product
             $mediaGalleryEntries = $product->getMediaGalleryEntries();
             $productData->setMediaGalleryEntries($mediaGalleryEntries);
 
