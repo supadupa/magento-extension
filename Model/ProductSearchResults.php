bcf975e9b1205a54d988d13fcfce33670099ad7b
Freddy McGroarty <freddymcgroarty@gmail.com>
2023-11-14 17:49:12 +0000

    Hotfix: PHP codesniff & composer deps
    * USING TEST CREDS *
    
    Also makes a significant change to Redirect/Index.php to allow for a
    magento safe way of decoding base64.
    
    Also adds correct deps into composer.json - this would have failed QA
    otherwise.

--- Model/ProductSearchResults.php
+++ Model/ProductSearchResults.php
@@ -4,18 +4,35 @@ namespace Sauce\App\Model;
 use Magento\Framework\Api\SearchResults;
 use Sauce\App\Api\Data\ProductSearchResultsInterface;
 
+/**
+ * Class ProductSearchResults
+ *
+ * This class extends the Magento SearchResults for custom handling of product search results.
+ */
 class ProductSearchResults extends SearchResults implements ProductSearchResultsInterface
 {
-  /**
-   * @var string
-   */
+    /**
+     * @var string
+     * Holds the version of the extension.
+     */
     protected $extensionVersion;
 
+    /**
+     * Retrieves the extension version.
+     *
+     * @return string The version of the extension.
+     */
     public function getExtensionVersion()
     {
         return $this->extensionVersion;
     }
 
+    /**
+     * Sets the extension version.
+     *
+     * @param string $version The version to be set for the extension.
+     * @return $this Provides a fluent interface.
+     */
     public function setExtensionVersion($version)
     {
         $this->extensionVersion = $version;
