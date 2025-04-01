812ce912fc57ef6b5318ff51308bbb977efc26d2
James Riley <james@codansa.com>
2025-04-01 12:04:55 +0100

    Switch to positional arguments (rather than named) for PHP 7.3 support

--- registration.php
+++ registration.php
@@ -3,7 +3,14 @@
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
-    type: ComponentRegistrar::MODULE,
-    componentName: 'Sauce_App',
-    path: __DIR__
+    /*
+     going with positional arguments, rather than named arguments,
+     to preserve support for PHP 7.3
+     type: ComponentRegistrar::MODULE,
+      componentName: 'Sauce_App',
+      path: __DIR__
+    */
+    ComponentRegistrar::MODULE,
+    'Sauce_App',
+    __DIR__
 );
